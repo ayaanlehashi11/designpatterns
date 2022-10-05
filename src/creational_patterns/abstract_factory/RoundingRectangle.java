b88b8ebf0f84570a1ffa93653e642cc58fca3167
ayaanle <ayaanlemohamed540@gmail.com>
2022-10-05 12:56:44 +0300

    designs

---
+++ src/creational_patterns/abstract_factory/RoundingRectangle.java
@@ -0,0 +1,8 @@
+package creational_patterns.abstract_factory;
+
+public class RoundingRectangle implements Shape {
+    @Override
+    public void draw() {
+
+    }
+}
---
+++ src/creational_patterns/abstract_factory/RoundingSquare.java
@@ -0,0 +1,8 @@
+package creational_patterns.abstract_factory;
+
+public class RoundingSquare implements Shape {
+    @Override
+    public void draw() {
+
+    }
+}
